a8c06c7f6cad3b387de2775101ecfd039679efd9
Eduardo5011 <eduardo.rosado5011@gmail.com>
2024-04-29 19:55:56 -0400

    updated

--- src/reducer.js
+++ src/reducer.js
@@ -9,7 +9,10 @@ import {
 
 
 const reducer = (state, action) => {
-  return state;
+  if(action.type === CLEAR_CART){
+     return {...state, cart: new Map()}
+    }
+  throw new Error(`no matching action type : ${action.type}`)
 };
 
 export default reducer;
